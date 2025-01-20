f191a9ec075b96fa4bdfebd6343baa61569405d2
ANAND <ANAND@FILM--1>
2025-01-20 12:27:20 +0800

    Add project files.

---
+++ Form2.Designer.vb
@@ -0,0 +1,387 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        ComboBox1 = New ComboBox()
+        RadioButton1 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        CheckBox1 = New CheckBox()
+        CheckBox2 = New CheckBox()
+        CheckBox3 = New CheckBox()
+        CheckBox4 = New CheckBox()
+        CheckBox5 = New CheckBox()
+        DataGridView1 = New DataGridView()
+        Column1 = New DataGridViewTextBoxColumn()
+        Column2 = New DataGridViewTextBoxColumn()
+        Column3 = New DataGridViewTextBoxColumn()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        RadioButton3 = New RadioButton()
+        RadioButton4 = New RadioButton()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 13.875F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(607, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(402, 50)
+        Label1.TabIndex = 0
+        Label1.Text = "Maklumat Pelanggan "
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(12, 130)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(96, 32)
+        Label2.TabIndex = 1
+        Label2.Text = "Nama : "
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(28, 172)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(97, 32)
+        Label3.TabIndex = 2
+        Label3.Text = "No.tel : "
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(28, 225)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(107, 32)
+        Label4.TabIndex = 3
+        Label4.Text = "Alamat : "
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(30, 266)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(105, 32)
+        Label5.TabIndex = 4
+        Label5.Text = "Negeri : "
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(27, 310)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(108, 32)
+        Label6.TabIndex = 5
+        Label6.Text = "Jantina : "
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(12, 354)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(203, 32)
+        Label7.TabIndex = 6
+        Label7.Text = "Kaedah Bayaran : "
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(14, 403)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(94, 32)
+        Label8.TabIndex = 7
+        Label8.Text = "Perisa : "
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(141, 222)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(200, 39)
+        TextBox2.TabIndex = 9
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(119, 172)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(200, 39)
+        TextBox3.TabIndex = 10
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(99, 127)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(200, 39)
+        TextBox4.TabIndex = 11
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(141, 267)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(242, 40)
+        ComboBox1.TabIndex = 12
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(151, 310)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(105, 36)
+        RadioButton1.TabIndex = 13
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "Lelaki"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(347, 313)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(166, 36)
+        RadioButton2.TabIndex = 14
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "Perempuan"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(798, 403)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(163, 36)
+        CheckBox1.TabIndex = 15
+        CheckBox1.Text = "CheckBox1"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox2
+        ' 
+        CheckBox2.AutoSize = True
+        CheckBox2.Location = New Point(606, 399)
+        CheckBox2.Name = "CheckBox2"
+        CheckBox2.Size = New Size(163, 36)
+        CheckBox2.TabIndex = 16
+        CheckBox2.Text = "CheckBox2"
+        CheckBox2.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox3
+        ' 
+        CheckBox3.AutoSize = True
+        CheckBox3.Location = New Point(437, 403)
+        CheckBox3.Name = "CheckBox3"
+        CheckBox3.Size = New Size(163, 36)
+        CheckBox3.TabIndex = 17
+        CheckBox3.Text = "CheckBox3"
+        CheckBox3.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox4
+        ' 
+        CheckBox4.AutoSize = True
+        CheckBox4.Location = New Point(268, 403)
+        CheckBox4.Name = "CheckBox4"
+        CheckBox4.Size = New Size(163, 36)
+        CheckBox4.TabIndex = 18
+        CheckBox4.Text = "CheckBox4"
+        CheckBox4.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox5
+        ' 
+        CheckBox5.AutoSize = True
+        CheckBox5.Location = New Point(99, 403)
+        CheckBox5.Name = "CheckBox5"
+        CheckBox5.Size = New Size(163, 36)
+        CheckBox5.TabIndex = 19
+        CheckBox5.Text = "CheckBox5"
+        CheckBox5.UseVisualStyleBackColor = True
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Columns.AddRange(New DataGridViewColumn() {Column1, Column2, Column3})
+        DataGridView1.Location = New Point(11, 438)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 82
+        DataGridView1.Size = New Size(758, 300)
+        DataGridView1.TabIndex = 20
+        ' 
+        ' Column1
+        ' 
+        Column1.HeaderText = "Bilangan"
+        Column1.MinimumWidth = 10
+        Column1.Name = "Column1"
+        Column1.Width = 200
+        ' 
+        ' Column2
+        ' 
+        Column2.HeaderText = "Nama"
+        Column2.MinimumWidth = 10
+        Column2.Name = "Column2"
+        Column2.Width = 200
+        ' 
+        ' Column3
+        ' 
+        Column3.HeaderText = "No.tel"
+        Column3.MinimumWidth = 10
+        Column3.Name = "Column3"
+        Column3.Width = 200
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(546, 761)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(150, 46)
+        Button1.TabIndex = 21
+        Button1.Text = "Delete"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(368, 761)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(150, 46)
+        Button2.TabIndex = 22
+        Button2.Text = "Update"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(191, 761)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(150, 46)
+        Button3.TabIndex = 23
+        Button3.Text = "Read"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(12, 761)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(150, 46)
+        Button4.TabIndex = 24
+        Button4.Text = "Create"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.AutoSize = True
+        RadioButton3.Location = New Point(406, 361)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(135, 36)
+        RadioButton3.TabIndex = 25
+        RadioButton3.TabStop = True
+        RadioButton3.Text = "Cashless"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton4
+        ' 
+        RadioButton4.AutoSize = True
+        RadioButton4.Location = New Point(210, 361)
+        RadioButton4.Name = "RadioButton4"
+        RadioButton4.Size = New Size(96, 36)
+        RadioButton4.TabIndex = 26
+        RadioButton4.TabStop = True
+        RadioButton4.Text = "Cash"
+        RadioButton4.UseVisualStyleBackColor = True
+        ' 
+        ' Form2
+        ' 
+        AutoScaleDimensions = New SizeF(13F, 32F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1645, 883)
+        Controls.Add(RadioButton4)
+        Controls.Add(RadioButton3)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(DataGridView1)
+        Controls.Add(CheckBox5)
+        Controls.Add(CheckBox4)
+        Controls.Add(CheckBox3)
+        Controls.Add(CheckBox2)
+        Controls.Add(CheckBox1)
+        Controls.Add(RadioButton2)
+        Controls.Add(RadioButton1)
+        Controls.Add(ComboBox1)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form2"
+        Text = "Form2"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox4 As CheckBox
+    Friend WithEvents CheckBox5 As CheckBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+End Class
